d0aba0e11241e89f9f3cbb347489ca85834bc56b
nyg <33351260@qq.com>
2025-01-22 00:32:35 +0800

    feat utils.py

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,20 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = [
+    "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"
+]
+# Put your name
+key_owner = "test_sim_ai"
+
+maze_assets_loc = "E:\\code\\python\\Town\\environment\\frontend_server\\static_dirs\\assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "E:\\code\\python\\Town\\environment\\frontend_server\\storage"
+fs_temp_storage = "E:\\code\\python\\Town\\environment\\frontend_server\\temp_storage"
+fs_back_end = "E:\\code\python\\Town\\reverie\\backend_server"
+collision_block_id = "32125"
+# api_url = "http://127.0.0.1:11434"
+emb_url = "http://127.0.0.1:11434"
+api_url = "http://127.0.0.1"
+# Verbose 
+debug = True
